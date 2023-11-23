67ae47c6e11fa1c50ce1860febfd82668684de10
w-ryan-jung <w.ryan.jung@gmail.com>
2023-11-23 18:00:32 +0900

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	modified:   tf_soldesk/tf_module_2/vpc/main.tf
    	modified:   tf_soldesk/tf_module_2/vpc/vars.tf

--- tf_soldesk/tf_module_2/vpc/main.tf
+++ tf_soldesk/tf_module_2/vpc/main.tf
@@ -1,5 +1,5 @@
 resource "aws_vpc" "vpc" {
-  cidr_block       = var.cidr_block
+  cidr_block       = var.cidr_block == null ? "10.0.0.0/16" : null
   enable_dns_hostnames = true
   tags = {
     Name = "${var.pjt_name}_vpc"
